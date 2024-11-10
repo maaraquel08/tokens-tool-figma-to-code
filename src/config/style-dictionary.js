3beb69c5e5fbaab40298a7650c9afaf6b1dfe1f5
Michael Anthony Raquel <raqanthony.2499@gmail.com>
2024-11-10 12:39:00 +0800

    Fully working theme switching of all colors
    Done working switching theme.

--- src/config/style-dictionary.js
+++ src/config/style-dictionary.js
@@ -1,6 +1,6 @@
 const StyleDictionary = require("style-dictionary");
 
-// Register a custom transform to handle references
+// Register custom transforms
 StyleDictionary.registerTransform({
     name: "resolve/color",
     type: "value",
@@ -15,7 +15,46 @@ StyleDictionary.registerTransform({
     },
 });
 
-// CSS Variables formatter
+// Color transform for Android
+StyleDictionary.registerTransform({
+    name: "color/android",
+    type: "value",
+    matcher: (token) => token.path[0] === "color",
+    transformer: (token) => {
+        // Convert hex to android color format
+        const hexToAndroid = (hex) => {
+            return hex.replace("#", "").toUpperCase();
+        };
+        return {
+            light: hexToAndroid(token.value.light),
+            dark: hexToAndroid(token.value.dark),
+        };
+    },
+});
+
+// Color transform for iOS
+StyleDictionary.registerTransform({
+    name: "color/ios",
+    type: "value",
+    matcher: (token) => token.path[0] === "color",
+    transformer: (token) => {
+        // Convert hex to UIColor format
+        const hexToUIColor = (hex) => {
+            const r = parseInt(hex.substr(1, 2), 16) / 255;
+            const g = parseInt(hex.substr(3, 2), 16) / 255;
+            const b = parseInt(hex.substr(5, 2), 16) / 255;
+            return `UIColor(red: ${r.toFixed(3)}, green: ${g.toFixed(
+                3
+            )}, blue: ${b.toFixed(3)}, alpha: 1.0)`;
+        };
+        return {
+            light: hexToUIColor(token.value.light),
+            dark: hexToUIColor(token.value.dark),
+        };
+    },
+});
+
+// Existing CSS Variables formatter
 StyleDictionary.registerFormat({
     name: "css/variables",
     formatter: function ({ dictionary }) {
@@ -43,7 +82,7 @@ StyleDictionary.registerFormat({
     },
 });
 
-// Tailwind theme formatter
+// Existing Tailwind formatter
 StyleDictionary.registerFormat({
     name: "javascript/tailwind",
     formatter: function ({ dictionary }) {
@@ -90,6 +129,63 @@ StyleDictionary.registerFormat({
     },
 });
 
+// Android Colors XML formatter
+StyleDictionary.registerFormat({
+    name: "android/colors",
+    formatter: function ({ dictionary }) {
+        const colorResources = dictionary.allTokens
+            .map((token) => {
+                const type = token.path[1];
+                const path = token.path.slice(2).join("_");
+                const resourceName = `color_${type}_${path}`;
+
+                return (
+                    `    <color name="${resourceName}_light">@{${token.value.light}}</color>\n` +
+                    `    <color name="${resourceName}_dark">@{${token.value.dark}}</color>`
+                );
+            })
+            .join("\n");
+
+        return (
+            `<?xml version="1.0" encoding="utf-8"?>\n` +
+            `<resources>\n${colorResources}\n</resources>`
+        );
+    },
+});
+
+// iOS Swift formatter
+StyleDictionary.registerFormat({
+    name: "ios/swift",
+    formatter: function ({ dictionary }) {
+        const colorExtension = dictionary.allTokens
+            .map((token) => {
+                const type = token.path[1];
+                const path = token.path.slice(2).join("_");
+                const varName = `${type}_${path}`;
+
+                return (
+                    `    static var ${varName}: UIColor {\n` +
+                    `        if #available(iOS 13.0, *) {\n` +
+                    `            return UIColor { traitCollection in\n` +
+                    `                switch traitCollection.userInterfaceStyle {\n` +
+                    `                case .dark:\n` +
+                    `                    return ${token.value.dark}\n` +
+                    `                default:\n` +
+                    `                    return ${token.value.light}\n` +
+                    `                }\n` +
+                    `            }\n` +
+                    `        } else {\n` +
+                    `            return ${token.value.light}\n` +
+                    `        }\n` +
+                    `    }`
+                );
+            })
+            .join("\n\n");
+
+        return `import UIKit\n\n` + `extension UIColor {\n${colorExtension}\n}`;
+    },
+});
+
 module.exports = {
     source: ["src/tokens/colors.json", "src/tokens/semantic-colors.json"],
     platforms: {
@@ -128,6 +224,38 @@ module.exports = {
                 {
                     destination: "theme.js",
                     format: "javascript/tailwind",
+                },
+            ],
+        },
+        android: {
+            transformGroup: "android",
+            transforms: [
+                "attribute/cti",
+                "name/cti/snake",
+                "color/android",
+                "resolve/color",
+            ],
+            buildPath: "dist/android/",
+            files: [
+                {
+                    destination: "colors.xml",
+                    format: "android/colors",
+                },
+            ],
+        },
+        ios: {
+            transformGroup: "ios",
+            transforms: [
+                "attribute/cti",
+                "name/cti/camel",
+                "color/ios",
+                "resolve/color",
+            ],
+            buildPath: "dist/ios/",
+            files: [
+                {
+                    destination: "UIColor+Theme.swift",
+                    format: "ios/swift",
                 },
             ],
         },
